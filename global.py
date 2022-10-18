1b39a9b9c4640f195abf21ea2a33a93bb38d2b05
PErameIn-GOleZafRon <90267145+PErameIn-GOleZafRon@users.noreply.github.com>
2022-10-18 22:14:28 +0400

    Add files via upload

--- global.py
+++ global.py
@@ -1,9 +1,11 @@
+import re
 import discord
 import os
 import traceback
 from discord.ext import commands
 import asyncio
 import datetime
+from numpy import False_
 import youtube_dl
 import random
 import sqlite3
@@ -19,7 +21,9 @@ PREFIX = "wg."
 intents = discord.Intents.all()
 bot = commands.AutoShardedBot(command_prefix=PREFIX, intents=intents)
 bot.remove_command('help')
-
+global STARTED
+STARTED = True
+staff = [690895078247497799, 956171634094518323]
 
 @bot.event
 async def on_ready():
@@ -27,14 +31,17 @@ async def on_ready():
     await bot.change_presence(status=discord.Status.dnd)
 
 global Gban
-GLOBAL_CHAT = 'global-chat'  # PEP8: Названия констант пишутся капсом
+GLOBAL_CHAT = 'global-chat1'  # PEP8: Названия констант пишутся капсом
 Gban = users
 @bot.event
 async def on_message(message):
     await bot.process_commands(message)
+    if STARTED == False:
+        return
     channel = discord.utils.get(message.guild.text_channels, name=GLOBAL_CHAT)
     if message.author.id == bot.user.id:
         return
+    if message.author.bot: return
     if message.channel.id != channel.id:
         return
     await message.delete() 
@@ -55,18 +62,23 @@ async def on_message(message):
 
 
 @bot.command()
-@commands.is_owner()
 async def ban(ctx, member: discord.Member, *, reason="Просто так"):
-	c = bot.get_channel(993070491428470834)
-	embed = discord.Embed(title=f"Пользователь {member} был за банен в глобальном чате по причине [{reason}]", color=discord.Color.from_rgb(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)), timestamp=datetime.datetime.utcnow())
-	with open ("bans.json", "r") as f:
-		users1 = json.load(f)
-	with open ("bans.json", "w") as f:
-		users1.append(member.id)
-		global Gban
-		Gban = users1
-		json.dump (users1, f)
-	await c.send(embed=embed)
+	if ctx.author.id in staff:
+		c = bot.get_channel(993273720040853577)
+		embed = discord.Embed(title=f"Пользователь {member} был за банен в глобальном чате по причине [{reason}]", color=discord.Color.from_rgb(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)), timestamp=datetime.datetime.utcnow())
+		await c.send(embed=embed)
+		embed = discord.Embed(title=f"Успешно", description=f"[Подробнее в канале оповещений глобала](https://discord.com/channels/993144849219993680/993273720040853577)", color=discord.Color.from_rgb(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)), timestamp=datetime.datetime.utcnow())
+		await ctx.send(embed=embed)
+		with open ("bans.json", "r") as f:
+			users1 = json.load(f)
+		with open ("bans.json", "w") as f:
+			users1.append(member.id)
+			global Gban
+			Gban = users1
+			json.dump (users1, f)
+	else:
+		embed = discord.Embed(title=f"Ошибка!\nВы не являетесь персоналом глобального чата, чтоб использовать эту команду!", color=discord.Color.from_rgb(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)), timestamp=datetime.datetime.utcnow())
+		await ctx.send(embed=embed)
 
 
 @bot.command()
@@ -75,14 +87,14 @@ async def ping(ctx):
 	await ctx.reply(embed=embed)
 
 @bot.command()
-@commands.has_permissions(manage_messages=True)
-@commands.cooldown(1, 5, commands.BucketType.guild)
 async def clear(ctx, amount=6):
-	if ctx.author.id == 690895078247497799:
+	if ctx.author.id in staff:
 		c = bot.get_channel(993273720040853577)
 		GLOBAL_CHAT = 'global-chat'
 		embed = discord.Embed(title=f"Отчистка глобального чата!\nСообщений отчищается [{amount}]", color=discord.Color.from_rgb(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)), timestamp=datetime.datetime.utcnow())
 		await c.send(embed=embed)
+		embed = discord.Embed(title=f"Успешно", description=f"[Подробнее в канале оповещений глобала](https://discord.com/channels/993144849219993680/993273720040853577)", color=discord.Color.from_rgb(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)), timestamp=datetime.datetime.utcnow())
+		await ctx.send(embed=embed)
 
 		for guild in bot.guilds:
 
@@ -94,20 +106,59 @@ async def clear(ctx, amount=6):
 		await ctx.send(embed=embed)
 
 @bot.command()
-@commands.is_owner()
 async def unban(ctx, member: discord.Member):
-	c = bot.get_channel(993070491428470834)
-	embed = discord.Embed(title=f"Пользователь {member} был за разбанен в глобальном чате по причине", color=discord.Color.from_rgb(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)), timestamp=datetime.datetime.utcnow())
-	with open ("bans.json", "r") as f:
-		users1 = json.load(f)
-	with open ("bans.json", "w") as f:
-		users1.remove(member.id)
-		global Gban
-		Gban = users1
-		json.dump (users1, f)
-	await c.send(embed=embed)
+	if ctx.author.id in staff:
+		c = bot.get_channel(993273720040853577)
+		embed = discord.Embed(title=f"Пользователь {member} был за разбанен в глобальном чате по причине", color=discord.Color.from_rgb(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)), timestamp=datetime.datetime.utcnow())
+		with open ("bans.json", "r") as f:
+			users1 = json.load(f)
+		with open ("bans.json", "w") as f:
+			users1.remove(member.id)
+			global Gban
+			Gban = users1
+			json.dump (users1, f)
+		await c.send(embed=embed)
+		embed = discord.Embed(title=f"Успешно", description=f"[Подробнее в канале оповещений глобала](https://discord.com/channels/993144849219993680/993273720040853577)", color=discord.Color.from_rgb(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)), timestamp=datetime.datetime.utcnow())
+		await ctx.send(embed=embed)
+	else:
+		embed = discord.Embed(title=f"Ошибка!\nВы не являетесь персоналом глобального чата, чтоб использовать эту команду!", color=discord.Color.from_rgb(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)), timestamp=datetime.datetime.utcnow())
+		await ctx.send(embed=embed)
 
+@bot.command()
+async def stop(ctx):
+	if ctx.author.id in staff:
+		global STARTED
+		STARTED = False
+		c = bot.get_channel(993273720040853577)
+		embed = discord.Embed(title=f"Глобальный чат был остановлен пользователем \n'{ctx.author} | {ctx.author.id}'", color=discord.Color.from_rgb(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)), timestamp=datetime.datetime.utcnow())
+		await c.send(embed=embed)
+		embed = discord.Embed(title=f"Успешно", description=f"[Подробнее в канале оповещений глобала](https://discord.com/channels/993144849219993680/993273720040853577)", color=discord.Color.from_rgb(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)), timestamp=datetime.datetime.utcnow())
+		await ctx.send(embed=embed)
 
+@bot.command()
+async def start(ctx):
+	if ctx.author.id in staff:
+		global STARTED
+		STARTED = True
+		c = bot.get_channel(993273720040853577)
+		embed = discord.Embed(title=f"Глобальный чат был запущен пользователем \n'{ctx.author} | {ctx.author.id}'", color=discord.Color.from_rgb(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)), timestamp=datetime.datetime.utcnow())
+		await c.send(embed=embed)
+		embed = discord.Embed(title=f"Успешно", description=f"[Подробнее в канале оповещений глобала](https://discord.com/channels/993144849219993680/993273720040853577)", color=discord.Color.from_rgb(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)), timestamp=datetime.datetime.utcnow())
+		await ctx.send(embed=embed)
+
+@bot.command()
+@commands.is_owner()
+async def msg(ctx, *, msg):
+    for guild in bot.guilds:
+        channel = bot.get_channel(bot.guild.text_channels, name=GLOBAL_CHAT)
+        if channel := discord.utils.get(guild.text_channels, name=GLOBAL_CHAT):
+            try:
+                embed = discord.Embed(title=f"Owner message", description = f'<:7371discordhypesquadbrilliance:1030520234051907674> <a:2353arrowrightglow:1029814234416169040>   **{msg}**', colour = ctx.author.color, timestamp=datetime.datetime.utcnow())
+                await ctx.send(embed = embed)
+            except discord.Forbidden:
+                print(f"Невозможно отправить сообщение на сервер {guild.name}: Недостаточно прав")
+            except discord.HTTPException as e:
+                print(f"Невозможно отправить сообщение на сервер {guild.name}: {e}")
 
 bot.run(TOKEN)
 
